5a61e42331c02b7c21aaae376d4e305ecf15c9f6
User <User@DESKTOP-GT59C8J>
2023-05-29 20:55:04 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            int number;
+            number = 12;
+
+            Console.WriteLine("tiv " + number);
+            int number = 12;
+            var nunufar = "somethinhg";
+
+            number = number * number;
+
+            Console.WriteLine(number);*/
+            /*Console.Write("Nermuceq tiv ");
+            double text = Convert.ToDouble(Console.Read());
+
+            text += 10;
+            Console.WriteLine(text);
+            Console.Write("I AM \"FDDF \" ");*/
+            /*int number = 5;
+            int result = number * 10;
+            Console.WriteLine(result);
+            Console.WriteLine(number);*/
+
+            /*
+                        Console.WriteLine("Enter a number1 ");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(a+1);*/
+            /*  Console.WriteLine("Input a number1");
+              int number1=Convert.ToInt32(Console.ReadLine());    
+              Console.WriteLine("Input a number2"); 
+              int number2=Convert.ToInt32(Console.ReadLine());*/
+
+            /*Console.WriteLine("Input a number3 ");
+                        int number3=Convert.ToInt32(Console.ReadLine());    
+                        if (number1 == number2 && number1==number3)
+                        {
+                            Console.WriteLine("The numbers are equal");
+                        }
+                        else if (number1 > number2 && number1>number3 ) {
+                            Console.WriteLine("maximum " + number1);
+                           }
+                        else if (number2>number1 && number2>number3 )
+                        {
+                            Console.Write("Maximum"+number2); 
+                        }
+                        else
+                        {
+                                Console.WriteLine("maximum"+number3);
+                        }
+                        if (number1==number2 && number2 == number3)
+                        {
+                            Console.WriteLine("equal");    
+                        }
+                        else if (number1<number2 && number1<number3)
+                        {
+                            Console.WriteLine("Minimum" + number1);
+
+                        }
+                        else if(number2<number1 && number2 < number3)
+                        {
+                            Console.WriteLine("Minimum" + number2);
+                        }
+                        else { Console.WriteLine("minimum " + number3); }*/
+            /*if (number1 == number2)
+            {
+                Console.WriteLine("equal");
+
+            }
+            else if (number1 > number2)
+            {
+                Console.WriteLine("maximum " + number1);
+
+            }
+            else
+            {
+                Console.WriteLine("maximum " + number2);
+
+            }
+            if (number1 == number2)
+                Console.WriteLine("EQUAL");
+            else if (number1 < number2)
+                Console.WriteLine("minimum " + number1);
+            else 
+                Console.WriteLine("minimum " + number2);
+            }*/
+            /*
+                        int[] num1 = new int[] { 1, 2, 6, 12, 42, 6 };
+                        int[] num2 = new int[] { 2, 3, 4, 5, 6, 2, 2 };
+                        int[] num3 = default;
+                        int index3 = 0;
+                        while (true)
+                        {
+                            for (int i = 0; i <= num1.Length; i++)
+                            {
+                                for (int j = 0; j < num2.Length; j++)
+
+                                    if (num1[i] == num2[j])
+                                    {
+                                        Console.WriteLine( num1[i]);
+                                    }
+                            }
+                        }
+                        index3++;
+                    }
+                        */
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+            while (a  <=10 & b<=10)
+            {
+
+               
+          
+
+            Console.WriteLine(a*b);
+             
+            }
+        }
+    }
+}
+
+    
